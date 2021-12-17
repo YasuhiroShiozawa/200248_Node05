68b367192ccb327418e05d9ed7d53d7c92ebd02a
YasuhiroShiozawa <200248nv@yse-c.net>
2021-12-17 11:51:49 +0900

    チャット機能関連の更新

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,21 @@
-$(()=>{
+$(() => {
     const url = ''
     let socket = io.connect(url)
 
     const message = $('#message')
     const chatlist = $('#chatlist')
 
-    $('#send').on('click',()=>{
+    $('#send').on('click', () => {
         if (!message.val()) return
 
-        socket.emit('message',{ message: message.val()})
-        
+        socket.emit('message', { message: message.val() })
+
         message.val('')
+    })
+
+    socket.on('message',(data)=>{
+        console.log(data)
+        let chatElement = $('<p>').append(data.message)
+        chatlist.prepend(chatElement)
     })
 })
